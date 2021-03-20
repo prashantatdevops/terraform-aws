aacc63a36b1dae765668d9cfb03c65a928de2285
Prashant Devops <prashantatdevops@outlook.com>
2021-03-21 01:39:18 +0530

    added s3 code

---
+++ modules/aws-s3/variables.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+  description = "missvidhyas3bucket901175"
+  type = string
+}
+
+variable "tags" {
+  description = "tes tag has been added"
+  type = map(string)
+  default = {}
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "access_key" {
+     description = "AKIAU5CNGJB3IOZXQ5ER"
+}
+variable "secret_key" {
+     description = "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"
+}
+variable "region" {
+     description = "us-east-1"
+}
+variable "bucket_name" {
+  description = "(Required) Creates a unique bucket name"
+  type        = "string"
+  default     = "test-bucket-vidhyaDevops"
+}
