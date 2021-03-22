7588334975e7b0008d8e574e622ffc051f7504f9
prashantatdevops <80918759+prashantatdevops@users.noreply.github.com>
2021-03-23 00:24:18 +0530

    Update networking.tf

--- modules/vpc/networking.tf
+++ modules/vpc/networking.tf
@@ -11,9 +11,9 @@ resource "aws_subnet" "main" {
   vpc_id     = "${var.vpc_id}"
   cidr_block = "${var.subnet_cidr}"
 
-  tags {
-    Name = "Main"
-  }
+#  tags {
+#    Name = "Main"
+#  }
 }
 
 output "vpc_id" {
