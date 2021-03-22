66fd12e68920b2f95eab035d4f7f9812ae0d91c4
prashantatdevops <80918759+prashantatdevops@users.noreply.github.com>
2021-03-23 01:06:13 +0530

    Update vars.tf

--- modules/ec2/vars.tf
+++ modules/ec2/vars.tf
@@ -2,7 +2,9 @@ variable "ec2_count" {
   default = "1"
 }
 
-variable "ami_id" {}
+variable "ami_id" {
+  default = "ami-0533f2ba8a1995cf9"
+  }
 
 variable "instance_type" {
   default = "t2.micro"
