8a678641025114779dae0baace62596da3950e03
prashantatdevops <80918759+prashantatdevops@users.noreply.github.com>
2021-03-23 01:03:39 +0530

    Update instances.tf

--- modules/ec2/instances.tf
+++ modules/ec2/instances.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web" {
   count         = "${var.ec2_count}"
-  #ami           = "${var.ami_id}"
-  #instance_type = "${var.instance_type}"
-  ami            = "ami-0533f2ba8a1995cf9"
-  instance_type  = "t2.micro"
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  #ami            = "ami-0533f2ba8a1995cf9"
+  #instance_type  = "t2.micro"
   subnet_id     = "${var.subnet_id}"
 
   #tags {
